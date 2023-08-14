5e76aa58c660567bc63355871a4563b2fd1fa3c7
adnankabir122 <adnankabir122@gmail.com>
2023-08-14 14:22:55 +0600

    second commit

---
+++ output/datatype.js
@@ -0,0 +1,15 @@
+"use strict";
+// built-in datatypes - number, string, boolean, void, undefined,null
+let userId;
+let userName;
+let isActive;
+userId = 101;
+//userId = '101' #wrong 
+userName = "Anisul islam";
+// userName = 209 #wrong 
+isActive = true;
+console.log(`UserName: ${userName} Id: ${userId} isActive: ${isActive}`);
+// String Methods 
+console.log(userName.split(" "));
+console.log(userName.toUpperCase());
+console.log(userName.toLowerCase());
---
+++ src/datatype.ts
@@ -0,0 +1,17 @@
+// built-in datatypes - number, string, boolean, void, undefined,null
+let userId:number;
+let userName: string;
+let isActive:boolean;
+
+userId = 101; 
+//userId = '101' #wrong 
+userName = "Anisul islam" 
+// userName = 209 #wrong 
+isActive = true;
+
+console.log(`UserName: ${userName} Id: ${userId} isActive: ${isActive}`)
+
+// String Methods 
+console.log(userName.split(" "))
+console.log(userName.toUpperCase())
+console.log(userName.toLowerCase())
