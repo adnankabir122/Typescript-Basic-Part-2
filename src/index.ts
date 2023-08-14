8c243fccc86a35670b5d468465e68237db0a1b14
adnankabir122 <adnankabir122@gmail.com>
2023-08-14 14:11:46 +0600

    second commit

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+// variable 
+// string type 
+let playerName = "Sakib";
+let age = 35;
+console.log(playerName);
+// upore jehetu playername - stringType chilo tai number assign kora jabe na 
+// playerName = 40; x
+// age = "hi" x
+
+// function 
+function multiply(a:number, b:number){
+    return a * b;
+}
+
+console.log(multiply(3,4));
+
+//array
+let fruits = ['apple','mango']
+// fruits.push(34) #number deya jabe na
+console.log(fruits)
+
+//object 
+let person ={
+    name: "sakib",
+    age : 35,
+    isCaptain : true
+}
+// person.age = '35' #string deya jabe na
+
